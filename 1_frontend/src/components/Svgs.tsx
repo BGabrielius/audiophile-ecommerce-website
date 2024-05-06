65d1f4e910f230b526b8ba343b525c01d3c966f7
BGabrielius <gabrieliusbatakis@gmail.com>
2024-05-06 21:16:40 +0300

    more svgs

--- 1_frontend/src/components/Svgs.tsx
+++ 1_frontend/src/components/Svgs.tsx
@@ -69,3 +69,17 @@ export const Instagram = () => {
     </svg>
   );
 };
+
+export const ArrowRight = () => {
+  return (
+    <svg width='8' height='12' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M1.322 1l5 5-5 5'
+        stroke='#D87D4A'
+        stroke-width='2'
+        fill='none'
+        fillRule='evenodd'
+      />
+    </svg>
+  );
+};
