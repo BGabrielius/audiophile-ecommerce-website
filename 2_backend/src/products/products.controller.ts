9a6b6677c323aa3bcfbf68cf5f564494ca59d561
BGabrielius <gabrieliusbatakis@gmail.com>
2024-06-05 19:12:09 +0300

    Initial Nestjs setup

---
+++ 2_backend/src/products/products.controller.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  /*
+    // :id = category of the products
+    // get all products from the category
+    GET /products/:id
+ -----------------------------------------------
+    // 1st -> :id = category of the products
+    // 2nd -> :id = product of the category
+    // get a single product from the category
+    Get /products/:id/:id
+    */
+}
