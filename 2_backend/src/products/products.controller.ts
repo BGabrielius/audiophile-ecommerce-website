838585ae691ef880dd5b0d46bc7c11af124f3651
BGabrielius <gabrieliusbatakis@gmail.com>
2024-06-06 22:14:10 +0300

    Possible routing <in progress>

--- 2_backend/src/products/products.controller.ts
+++ 2_backend/src/products/products.controller.ts
@@ -1,15 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
   /*
     // :id = category of the products
     // get all products from the category
-    GET /products/:id
- -----------------------------------------------
+    
+    */
+  @Get(':id') // GET /products/:id
+  findAllByCategory(
+    @Query('category') category: 'HEADPHONES' | 'SPEAKERS' | 'EARPHONES',
+  ) {
+    return this.productsService.findAllByCategory(category);
+  }
+  // -----------------------------------------------
+  /*
     // 1st -> :id = category of the products
     // 2nd -> :id = product of the category
     // get a single product from the category
-    Get /products/:id/:id
+    
     */
+  @Get(':category/:id') // Get /products/:id/:id
+  findOne(
+    @Param('category') category: 'HEADPHONES' | 'SPEAKERS' | 'EARPHONES',
+    @Param('id') id: string,
+  ) {
+    return this.productsService.findOne(category, id);
+  }
 }
--- 2_backend/src/products/products.service.ts
+++ 2_backend/src/products/products.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private products = [];
+  findAllByCategory(category: 'HEADPHONES' | 'SPEAKERS' | 'EARPHONES') {
+    return this.products.find((product) => product.category === category);
+  }
+  findOne(category: 'HEADPHONES' | 'SPEAKERS' | 'EARPHONES', id: string) {
+    const categorized = this.products.find(
+      (product: any) => product.category === category,
+    );
+
+    return categorized.find((product: any) => product.id === id);
+  }
+}
