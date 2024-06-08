dba1d37bdfc714d7b2010e098c44fe5056a3675c
BGabrielius <gabrieliusbatakis@gmail.com>
2024-06-08 19:30:41 +0300

    accessability features & restructured to use the same data source

--- 1_frontend/src/components/YouMayAlsoLikeSection.tsx
+++ 1_frontend/src/components/YouMayAlsoLikeSection.tsx
@@ -3,47 +3,13 @@
 import { useEffect, useState } from 'react';
 import Button from './Button';
 import { useRouter } from 'next/navigation';
-interface Product {
-  title: string;
-  img: string;
-  route: string;
-}
-const products: Product[] = [
-  {
-    title: 'XX99 MARK II',
-    img: 'xx99-mark-two-headphones',
-    route: '/headphones/xx99II',
-  },
-  {
-    title: 'XX99 MARK I',
-    img: 'xx99-mark-one-headphones',
-    route: '/headphones/xx99I',
-  },
-  {
-    title: 'XX59',
-    img: 'xx59-headphones',
-    route: '/headphones/xx59',
-  },
-  {
-    title: 'ZX9',
-    img: 'zx9-speaker',
-    route: '/speakers/zx9',
-  },
-  {
-    title: 'ZX7',
-    img: 'zx7-speaker',
-    route: '/speakers/zx7',
-  },
-  {
-    title: 'YX1',
-    img: 'yx1-earphones',
-    route: '/earphones/yx1',
-  },
-];
+
+import { products, ProductInfo } from '@/tempData';
+
 const YouMayAlsoLikeSection: React.FC<{ currProduct: string }> = ({
   currProduct,
 }) => {
-  const [randomProducts, setRandomProducts] = useState<Product[]>();
+  const [randomProducts, setRandomProducts] = useState<ProductInfo[]>();
 
   const router = useRouter();
 
@@ -57,16 +23,16 @@ const YouMayAlsoLikeSection: React.FC<{ currProduct: string }> = ({
     const filteredProducts = products.filter(
       (product) => product.title !== currProduct
     );
-    const randomItems: Product[] = filteredProducts
+    const randomItems: ProductInfo[] = filteredProducts
       .sort(() => 0.5 - Math.random())
       .slice(0, 3);
     setRandomProducts(randomItems);
   };
   return (
     <section className='w-full'>
-      <h3 className='text-center mb-10 2sm:mb-14 2xl:mb-16 font-bold text-[24px] leading-h3 tracking-[0.86px] 2sm:text-h3 2sm:tracking-h3'>
+      <h4 className='text-center mb-10 2sm:mb-14 2xl:mb-16 font-bold text-[24px] leading-h3 tracking-[0.86px] 2sm:text-h3 2sm:tracking-h3'>
         YOU MAY ALSO LIKE
-      </h3>
+      </h4>
       <div className='w-full flex flex-col 2sm:flex-row gap-14 2sm:gap-[11px] 2xl:gap-[30px]'>
         {randomProducts &&
           randomProducts.map((product) => (
@@ -77,6 +43,7 @@ const YouMayAlsoLikeSection: React.FC<{ currProduct: string }> = ({
               <img
                 src={`/assets/product-${product.img}/desktop/image-category-page-preview.jpg`}
                 className='w-full rounded-[8px] 2sm:mb-2 2sm:max-h-[318px]'
+                alt={`${product.title} ${product.type} product`}
               />
 
               <h5 className='font-bold text-h5 tracking-h5 leading-h5'>
@@ -85,7 +52,9 @@ const YouMayAlsoLikeSection: React.FC<{ currProduct: string }> = ({
               <Button
                 text='SEE PRODUCT'
                 type='primary'
-                action={() => router.replace(product.route)}
+                action={() =>
+                  router.replace(`/${product.categoryType}/${product.route}`)
+                }
               />
             </div>
           ))}
