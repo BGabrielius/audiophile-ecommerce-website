58af710e0303080f7ec0db814c351238f779121a
BGabrielius <gabrieliusbatakis@gmail.com>
2024-06-14 21:11:28 +0300

    changed the product/s data input source

--- 1_frontend/src/components/ComponentWrappers/ProductCategoryContentWrapper.tsx
+++ 1_frontend/src/components/ComponentWrappers/ProductCategoryContentWrapper.tsx
@@ -1,32 +1,53 @@
-import { products, ProductInfo } from '@/tempData';
+'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProductsNav from '../ProductsNav';
 import AudioGearSection from '../AudioGearSection';
 import CategoryProduct from '../CategoryProduct';
 import Headline from '../Headline';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/redux/store';
+import {
+  ProductInfo,
+  getCategorizedProducts,
+} from '@/redux/Products/productsSlice';
 
 const ProductCategoryContentWrapper: React.FC<{ category: string }> = ({
   category,
 }) => {
+  const products = useSelector((state: RootState) => state.products.products);
+  const message = useSelector((state: RootState) => state.products.message);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (!products || products[0].category !== category) {
+      dispatch(getCategorizedProducts(category));
+    }
+    if (products) console.log(products);
+  }, [products]);
+
   return (
     <main className='flex flex-col gap-[120px]'>
-      <Headline headline='HEADPHONES' />
+      <Headline
+        headline={message ? message.toUpperCase() : category.toUpperCase()}
+      />
       <section className='flex flex-col gap-[120px] 2xl:gap-[160px]'>
-        {products.map(
-          (product: ProductInfo) =>
-            product.categoryType === category && (
-              <CategoryProduct
-                img={product.img}
-                title={product.title}
-                description={product.description}
-                isNew={product.isNew}
-                swap={product.swap}
-                key={product.title}
-                action={`/${product.categoryType}/${product.route}`}
-              />
-            )
-        )}
+        {products &&
+          products.map(
+            (product: ProductInfo) =>
+              product.category === category && (
+                <CategoryProduct
+                  img={product.slug}
+                  title={product.name}
+                  description={product.description}
+                  isNew={product.new}
+                  swap={product.swap}
+                  key={product.name}
+                  action={`/${product.category}/${product.route}`}
+                />
+              )
+          )}
       </section>
       <section className='w-full'>
         <ProductsNav />
--- 1_frontend/src/components/ComponentWrappers/ProductContentWrapper.tsx
+++ 1_frontend/src/components/ComponentWrappers/ProductContentWrapper.tsx
@@ -9,80 +9,97 @@ import YouMayAlsoLikeSection from '../YouMayAlsoLikeSection';
 import Gallery from '../Gallery';
 import ProductFeatures from '../ProductFeatures';
 import Product from '../Product';
-import { ProductInfo, products } from '@/tempData';
 import { useRouter } from 'next/navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/redux/store';
+import {
+  Product as IProduct,
+  getOneProduct,
+  getOneProductParams,
+} from '@/redux/Products/productsSlice';
 
 const ProductContentWrapper: React.FC<{ params: string; category: string }> = ({
   params,
   category,
 }) => {
-  const [productInfo, setProductInfo] = useState<ProductInfo>();
   const [pageNotFound, setPageNotFound] = useState<boolean>(false);
 
   const router = useRouter();
 
-  useEffect(() => {
-    selectedProduct();
-  }, []);
+  const product: IProduct | null = useSelector(
+    (state: RootState) => state.products.product
+  );
+  const message = useSelector((state: RootState) => state.products.message);
 
-  const selectedProduct = () => {
-    let productFound: boolean = false;
-    for (let i = 0; i < products.length; i++) {
-      if (
-        products[i].route === params &&
-        products[i].categoryType === category
-      ) {
-        setProductInfo(products[i]);
-        productFound = true;
-      }
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const getOneParams: getOneProductParams = {
+      category: category,
+      productId: params,
+    };
+    if (!product || product.about.route !== params) {
+      dispatch(getOneProduct(getOneParams));
     }
-    if (!productFound) setPageNotFound(true);
-  };
+    if (product) console.log(product);
+  }, [product]);
+
+  // const selectedProduct = () => {
+  //   let productFound: boolean = false;
+  //   for (let i = 0; i < products.length; i++) {
+  //     if (
+  //       products[i].route === params &&
+  //       products[i].categoryType === category
+  //     ) {
+  //       productFound = true;
+  //     }
+  //   }
+  //   if (!productFound) setPageNotFound(true);
+  // };
   return (
     <>
-      {products.map(
-        (product: ProductInfo) =>
-          product.categoryType === category &&
-          product.route === params && (
-            <main className='flex justify-start gap-[120px]' key={params}>
-              <section className='w-full flex items-start mb-[-96px] 2xl:mb-[-64px] mt-4 md:mt-8 2xl:mt-20'>
-                <button
-                  className='text-body leading-body opacity-60 self-start hover:text-orange-clay'
-                  onClick={() => router.back()}
-                >
-                  Go Back
-                </button>
-              </section>
-              <Product
-                img={product.img}
-                title={product.title}
-                type={product.type}
-                description={product.description}
-                price={product.price}
-                isNew={product.isNew}
-              />
-              <section className='flex flex-col gap-[120px] 2xl:flex-row 2xl:justify-between'>
-                <ProductFeatures
-                  p1={product.ProductFeaturesP1}
-                  p2={product.ProductFeaturesP2}
-                />
-                <InTheBox
-                  type={product.type}
-                  children={product.inTheBoxChildren}
-                />
-              </section>
-              <Gallery
-                img={product.img}
-                title={product.title}
-                type={product.type}
-              />
-              <YouMayAlsoLikeSection currProduct={product.title} />
+      (
+      {product ? (
+        <main className='flex justify-start gap-[120px]' key={params}>
+          <section className='w-full flex items-start mb-[-96px] 2xl:mb-[-64px] mt-4 md:mt-8 2xl:mt-20'>
+            <button
+              className='text-body leading-body opacity-60 self-start hover:text-orange-clay'
+              onClick={() => router.back()}
+            >
+              Go Back
+            </button>
+          </section>
+          <Product
+            img={product.about.slug}
+            title={product.about.name}
+            description={product.about.description}
+            price={product.about.price}
+            isNew={product.about.new}
+          />
+          <section className='flex flex-col gap-[120px] 2xl:flex-row 2xl:justify-between'>
+            <ProductFeatures
+              p1={product.about.features}
+              p2={product.about.features}
+            />
+            <InTheBox
+              type={product.about.category}
+              children={product.items.item}
+            />
+          </section>
+          <Gallery
+            img={product.about.slug}
+            title={product.about.name}
+            type={product.about.category}
+          />
+          <YouMayAlsoLikeSection currProduct={product.about.name} />
 
-              <ProductsNav />
-              <AudioGearSection />
-            </main>
-          )
+          <ProductsNav />
+          <AudioGearSection />
+        </main>
+      ) : (
+        message
       )}
+      )
       {pageNotFound && (
         <main className='flex justify-start'>
           <Headline headline='PAGE NOT FOUND' />{' '}
