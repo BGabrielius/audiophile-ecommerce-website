f5a678d6f8c363503b515ef67b7292876a9a8a44
BGabrielius <gabrieliusbatakis@gmail.com>
2024-06-21 23:03:35 +0300

    dynamic important with server side rendering disabled

--- 1_frontend/src/app/earphones/[id]/page.tsx
+++ 1_frontend/src/app/earphones/[id]/page.tsx
@@ -1,5 +1,10 @@
-import ProductContentWrapper from '@/components/ComponentWrappers/ProductContentWrapper';
-
+import dynamic from 'next/dynamic';
+export const ProductContentWrapper = dynamic(
+  () => import('@/components/ComponentWrappers/ProductContentWrapper'),
+  {
+    ssr: false,
+  }
+);
 const page = ({ params }: { params: { id: string } }) => {
   return <ProductContentWrapper params={params.id} category='earphones' />;
 };
