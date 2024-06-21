ac742aeece237a4529063f94f47405496446a100
BGabrielius <gabrieliusbatakis@gmail.com>
2024-06-21 23:04:10 +0300

    YouMayAlsoLikeSection fixes with the new data input <in progress>

--- 1_frontend/src/components/YouMayAlsoLikeSection.tsx
+++ 1_frontend/src/components/YouMayAlsoLikeSection.tsx
@@ -1,59 +1,41 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import Button from './Button';
 import { useRouter } from 'next/navigation';
 
-import { products, ProductInfo } from '@/tempData';
+import { ProductInfo } from '@/redux/Products/productsSlice';
 
-const YouMayAlsoLikeSection: React.FC<{ currProduct: string }> = ({
-  currProduct,
+const YouMayAlsoLikeSection: React.FC<{ uniqueProducts: ProductInfo[] }> = ({
+  uniqueProducts,
 }) => {
-  const [randomProducts, setRandomProducts] = useState<ProductInfo[]>();
-
   const router = useRouter();
 
-  useEffect(() => {
-    if (!randomProducts) {
-      removeSelectedProduct();
-    }
-  }, []);
-
-  const removeSelectedProduct = () => {
-    const filteredProducts = products.filter(
-      (product) => product.title !== currProduct
-    );
-    const randomItems: ProductInfo[] = filteredProducts
-      .sort(() => 0.5 - Math.random())
-      .slice(0, 3);
-    setRandomProducts(randomItems);
-  };
   return (
     <section className='w-full'>
       <h4 className='text-center mb-10 2sm:mb-14 2xl:mb-16 font-bold text-[24px] leading-h3 tracking-[0.86px] 2sm:text-h3 2sm:tracking-h3'>
         YOU MAY ALSO LIKE
       </h4>
       <div className='w-full flex flex-col 2sm:flex-row gap-14 2sm:gap-[11px] 2xl:gap-[30px]'>
-        {randomProducts &&
-          randomProducts.map((product) => (
+        {uniqueProducts &&
+          uniqueProducts.map((product) => (
             <div
               className='w-full flex flex-col items-center gap-8'
-              key={product.title}
+              key={product.name}
             >
               <img
-                src={`/assets/product-${product.img}/desktop/image-category-page-preview.jpg`}
+                src={`/assets/product-${product.slug}/desktop/image-category-page-preview.jpg`}
                 className='w-full rounded-[8px] 2sm:mb-2 2sm:max-h-[318px]'
-                alt={`${product.title} ${product.type} product`}
+                alt={`${product.name} ${product.category} product`}
               />
 
               <h5 className='font-bold text-h5 tracking-h5 leading-h5'>
-                {product.title}
+                {product.name}
               </h5>
               <Button
                 text='SEE PRODUCT'
                 type='primary'
                 action={() =>
-                  router.push(`/${product.categoryType}/${product.route}`)
+                  router.push(`/${product.category}/${product.route}`)
                 }
               />
             </div>
